7a2fd8a1fd7d473fef6ef5e935ac2d02df8f89ed
EdgarFigueiras <figueirasgomez@gmail.com>
2017-11-27 12:54:32 +0100

    3D psi matrix supported

--- C_array.c
+++ C_array.c
@@ -277,6 +277,59 @@ static PyObject* MatrixProb2D(PyObject* self, PyObject* args)
 	return PyArray_Return(matout);
 }
 
+/* ==== Operate on Matrix components  =========================
+   Access to the introduced matrix data to calculate 3d points using MonteCarlo funciont with random numbers
+   matin now is a 3D array like this matin[x][y][z] with the psi values
+    Sets the values in the inserted matout NumPy array and gives 3d space points and the probability [4]
+    interface:  matrix3Dprob(matin, matout)
+                matin is the psi values NumPy matrix, matout is the output values matrix(matrix[number_points][4] )
+*/
+static PyObject* matrix3Dprob(PyObject* self, PyObject* args)
+{
+    PyArrayObject *matin, *matout;
+    double **cout;   
+    int n = 0;                      //If 0 at debug then something failed, else returns 1
+    int f_in, c_in, e_in, f_out, c_out;
+
+    if (!PyArg_ParseTuple(args, "OO", &matin, &matout))
+        return NULL;
+
+    cout=pymatrix_to_Carrayptrs(matout);
+
+    f_in=matin->dimensions[0];
+    c_in=matin->dimensions[1];
+    e_in=matin->dimensions[2];
+
+    f_out=matout->dimensions[0];
+    c_out=matout->dimensions[1];
+
+    double random = 0;
+    int rand_x = 0;
+    int rand_y = 0;
+    int rand_z = 0;
+
+    for (int i=0; i<f_out; i++)  {
+        random = randomInRange(0,1.0);
+        rand_x = (int) randomInRange(0,f_in);
+        rand_y = (int) randomInRange(0,c_in);
+        rand_z = (int) randomInRange(0,e_in);
+        while (random > *((double *)PyArray_GETPTR3(matin,rand_x,rand_y,rand_z)))
+        {
+            random = randomInRange(0,1);
+            rand_x = (int) randomInRange(0,f_in);
+            rand_y = (int) randomInRange(0,c_in);
+            rand_z = (int) randomInRange(0,e_in);
+        }
+        cout[i][0] = rand_x - f_in/2 + randomInRange(0,1); //with this substraction operation values are better balanced
+        cout[i][1] = rand_y - c_in/2 + randomInRange(0,1); //because the final objective is show them in a 3D grid
+        cout[i][2] = rand_z - e_in/2 + randomInRange(0,1); //now this value is real too, because comes from 3D data
+        cout[i][3] = *((double *)PyArray_GETPTR3(matin,rand_x,rand_y,rand_z));
+    }
+
+    n=1;
+    return Py_BuildValue("i", n);
+}
+
 
 static PyObject* version(PyObject* self)
 {
@@ -286,6 +339,7 @@ static PyObject* version(PyObject* self)
 static PyMethodDef C_array_Methods[] = {
     {"Matrix2D", Matrix2D, METH_VARARGS, "Calculate the matrix values."},
     {"MatrixProb2D", MatrixProb2D, METH_VARARGS, "Calculate the matrix values and return probability too."},
+    {"matrix3Dprob", matrix3Dprob, METH_VARARGS, "Calculates the MonteCarlo with the imput array (3d) and saves results in the other array (ArrayImput, ArrayOutput) returns probability too ArrayOutput[any_size][4]"},
     {"version", (PyCFunction)version, METH_NOARGS, "Returns the version."},
     {NULL, NULL, 0, NULL}
 };
